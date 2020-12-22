79683bab3996fa7c11160fe72c89595aa188412c
kaloyanbobev <kaloyanbobev@gmail.com>
2020-12-22 14:13:07 +0000

    add nav and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import react from 'react';
-import { Link, Route } from 'react-router-dom';
+
+import { Link, Route, Switch } from 'react-router-dom';
 
 import './App.scss';
 import Home from './pages/Home';
+import ForUs from './pages/ForUs';
+import Contacts from './pages/Contacts';
+
 
 function App() {
   return (
     <div className="app">
-      <Route to="/" component={Home}></Route>
+      <Link to="/" > Начало </Link>
+      <Link to="/for-us" > За нас </Link>
+      <Link to="/contacts" > Контакти </Link>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/for-us" component={ForUs} />
+        <Route path="/contacts" component={Contacts} />
+      </Switch>
     </div>
   );
 }
