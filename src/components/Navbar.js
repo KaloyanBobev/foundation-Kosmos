7777e9e6c9f8bed0d5c028cd1bb0b78845134c5e
kaloyanbobev <kaloyanbobev@gmail.com>
2020-12-23 19:52:55 +0000

    add isOpen state and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { FaAlignRight } from 'react-icons/fa';
 export default class Navbar extends Component {
+
+    state = {
+        isOpen: false
+    }
+
+    handleToggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+        console.log(this.state.isOpen);
+    }
+
     render() {
         return (
-            <>
-                <Link to="/" > Начало </Link>
-                <Link to="/about" > За нас </Link>
-                <Link to="/contacts" > Контакти </Link>
-            </>
+            <div className="navbar">
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/" className="logo"><strong>Ф о н д а ц и я    К о с м о с </strong></Link>
+                        <Link to="/" > Начало </Link>
+                        <Link to="/about" > За нас </Link>
+                        <Link to="/contacts" > Контакти </Link>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
