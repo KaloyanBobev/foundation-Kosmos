fb72f4a73b3e58460b928a5234f67e32dd7ead36
kaloyanbobev <kaloyanbobev@gmail.com>
2020-12-27 18:03:17 +0000

    create responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,16 +16,32 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <div className="navbar">
+            <nav className="navbar">
                 <div className="nav-center">
                     <div className="nav-header">
-                        <Link to="/" className="logo"><strong>Ф о н д а ц и я    К о с м о с </strong></Link>
-                        <Link to="/" > Начало </Link>
-                        <Link to="/about" > За нас </Link>
-                        <Link to="/contacts" > Контакти </Link>
+                        <Link to="/" className="logo">Ф о н д а ц и я    К о с м о с</Link>
+                        <button type="button" className="nav-btn">
+                            <FaAlignRight
+                                className="nav-icon"
+                                onClick={this.hadleToggle} />
+                        </button>
                     </div>
+                    <ul className={this.state.isOpen
+                        ? "nav-links show-nav"
+                        : "nav-links"}>
+                        <li>
+                            <Link to="/" > Начало </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" > За нас </Link>
+                        </li>
+                        <li>
+                            <Link to="/contacts" > Контакти </Link>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+            </nav>
+
         )
     }
 }
