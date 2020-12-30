a6d367b1839c8b9de8a6ec78bb38d662a7f103e0
kaloyanbobev <kaloyanbobev@gmail.com>
2020-12-30 21:23:56 +0000

    add gallery page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contacts from './pages/Contacts';
 import NotFound from './pages/NotFound';
+import Gallery from './pages/Gallery';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/gallery" component={Gallery} />
         <Route path="/contacts" component={Contacts} />
         <Route component={NotFound} />
       </Switch>
