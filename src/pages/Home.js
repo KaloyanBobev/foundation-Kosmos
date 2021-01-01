e7974a6501f6f4599a9161540079423ae76df0b2
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-01 18:46:47 +0000

    add Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import Hero from '../components/Hero';
+import image from '../images/Cosmos-hero-1.jpg';
+
 export default class Home extends Component {
     render() {
         return (
             <div>
-                Начало
+                <Hero image={image} />
             </div>
         )
     }
