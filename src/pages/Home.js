b30e18a69c86d2f872c68a26604ac43542a713d2
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-02 21:19:55 +0000

    add hero props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 
 import Hero from '../components/Hero';
-import image from '../images/Cosmos-hero-1.jpg';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <Hero image={image} />
+                <Hero hero={"defaultHero"} />
             </div>
         )
     }
