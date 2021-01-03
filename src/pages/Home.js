6668243a78c3cca7030f25f1cfee1cdd8888341c
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-03 11:24:40 +0000

    remove some files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 
-import Hero from '../components/Hero';
-
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <Hero hero={"defaultHero"} />
+                Home
             </div>
         )
     }
