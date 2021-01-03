ef3aaa2795ec1c2051cb93ebb8d99a149b4449c9
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-03 18:27:43 +0000

    create Poets page

---
+++ src/pages/Poets.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Poets extends Component {
+    render() {
+        return (
+            <div>
+                Poets
+            </div>
+        )
+    }
+}
