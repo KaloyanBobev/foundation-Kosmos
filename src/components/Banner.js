16b7403f96c3d342a20b34035f42fb926fbfc8cb
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-03 18:26:52 +0000

    create Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Banner extends Component {
+    render() {
+        return (
+            <>
+                <div className="banner-container">
+                    <div className="banner-center">
+                        <div className="banner-text">
+                            <h1>Фондация Космос</h1>
+                            <p>Подзаглавие</p>
+                            <p> Линк</p>
+                        </div>
+                    </div>
+                </div>
+
+            </>
+        )
+    }
+}
