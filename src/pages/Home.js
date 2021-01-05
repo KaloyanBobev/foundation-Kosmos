a616b198f3863e4dff6c2eefdf1122a53c52d0ac
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-05 17:57:49 +0000

    add visible,handleTogle abd toggle btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 
-import Data from '../data/data';
+import homeData from '../data/homeData';
+import HomeContainer from '../components/HomeContainer';
 export default class Home extends Component {
     constructor() {
         super();
         this.state = {
-            data: Data
+            visible: true,
+            data: []
         }
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+    componentDidMount() {
+        this.setState({
+            data: homeData
+        })
+
+    }
+
+    handleToggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+
     }
 
     render() {
         return (
-            <section className="home-container">
-                <h1>Учредиха клуб “Космос” в резиденция “Змейово”</h1>
-                {this.state.data.map((item, i) => {
-                    return (
-                        <p key={item.sys.id}>
-                            {item.fields.text}
-                        </p>
-                    )
-                })}
-            </section>
+            <>
+                <section className={this.state.visible ? "home-container" : "home-container show"}>
+                    <h1>Учредиха клуб “Космос” в резиденция “Змейово”</h1>
+                    <HomeContainer data={this.state.data} />
+                </section>
+                <button className="btn-primary btn" onClick={this.handleToggle}>повече</button>
+            </>
         )
     }
 }
+
+
