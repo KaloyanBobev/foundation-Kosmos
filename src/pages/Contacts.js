eacb151450be1cb27c4817f94ea2849359468bde
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-06 16:49:15 +0000

    add some html

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Contacts() {
     return (
-        <div>
-            Контакти
-        </div>
+        <section className="contacts-container">
+            <h1>Фондация Космос</h1>
+            <h3>address</h3>
+            <h3>tel.</h3>
+            <h3>mob.</h3>
+            <h2>Contact form</h2>
+        </section>
     )
 }
