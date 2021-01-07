d0474280eca11d453b44a43105d026887cf0c6a5
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-07 13:28:20 +0000

    fill the html

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import img from '../images/not-found-image.jpg';
 
 export default function NotFound() {
     return (
-        <div>
-            Not Found
-        </div>
+        <section className="not-found">
+            <h1>Whoops! </h1>
+            <h3>404 Page Not Found</h3>
+            <p>Looks like this page is lost in open cosmos</p>
+            <img src={img} alt="not found" />
+            <p>Try our <Link to="/" >homepage</Link> to back to planet Earth</p>
+        </section>
     )
 }
