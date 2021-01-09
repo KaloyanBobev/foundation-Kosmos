3fce0e4d9c9f0c5cc6e7feafe0435ae8d5fefd38
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-09 17:03:32 +0000

    create the layout for this page

--- src/pages/Poets.js
+++ src/pages/Poets.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
+import img from '../images/esteblishment.jpg';
+
 export default class Poets extends Component {
     render() {
         return (
-            <div>
-                Poets
-            </div>
+            <section className="poets-container">
+                <h1>Учередяване на дружеството на писателите</h1>
+
+                <a href={img} className="image-container">
+                    <img src={img} alt="esteblishment" ></img>
+                </a>
+
+                <a href="https://www.facebook.com/groups/487029208877556"
+                    className="facebook-link">
+                    Facebook страница: Приятели на списание "Птици в нощта"
+                    </a>
+
+            </section>
         )
     }
 }
