1fd899892d401d152bb329b6dd9e4ffb94ad5bca
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-09 17:01:30 +0000

    addd footer component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Banner from './components/Banner';
 import Home from './pages/Home';
 import Books from './pages/Books';
 import Poets from './pages/Poets';
-import About from './pages/About';
+// import About from './pages/About';
 import Contacts from './pages/Contacts';
 import NotFound from './pages/NotFound';
 import Gallery from './pages/Gallery';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -21,11 +22,12 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/poets" component={Poets} />
-        <Route path="/about" component={About} />
+        {/* <Route path="/about" component={About} /> */}
         <Route path="/gallery" component={Gallery} />
         <Route path="/contacts" component={Contacts} />
         <Route component={NotFound} />
       </Switch>
+      <Footer />
     </div>
   );
 }
