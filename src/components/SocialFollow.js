4c2f478c029206deefaff7f5e5a58f4922d6b8b5
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-11 14:47:43 +0000

    create social media component

---
+++ src/components/SocialFollow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function SocialFollow() {
+    return (
+        <div className="social-container">
+            <h3>Social Follow</h3>
+            <a href="https://www.youtube.com/"
+                className="youtube social"
+                target="_blank">
+                <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </a>
+            <a href="https://www.facebook.com/groups/487029208877556/"
+                className="facebook social"
+                target="_blank">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a href="https://www.twitter.com/"
+                className="twitter social"
+                target="_blank">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+            <a href="https://www.instagram.com/"
+                className="instagram social"
+                target="_blank">
+
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+        </div>
+    )
+}
