b1bb1bbc617460f466f468982b9f5c1e439cbd74
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-11 15:05:58 +0000

    add about route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Banner from './components/Banner';
 import Home from './pages/Home';
 import Books from './pages/Books';
 import Poets from './pages/Poets';
-// import About from './pages/About';
+import About from './pages/About';
 import Contacts from './pages/Contacts';
 import NotFound from './pages/NotFound';
 import Gallery from './pages/Gallery';
@@ -22,7 +22,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/books" component={Books} />
         <Route exact path="/poets" component={Poets} />
-        {/* <Route path="/about" component={About} /> */}
+        <Route path="/about" component={About} />
         <Route path="/gallery" component={Gallery} />
         <Route path="/contacts" component={Contacts} />
         <Route component={NotFound} />
