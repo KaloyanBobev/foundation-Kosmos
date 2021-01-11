e12e23d1a2ee7f5348909cd732ff5b7bf6e10b01
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-11 14:48:13 +0000

    move the all code from book page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-export default function About() {
-    return (
-        <div>
-            За нас
-        </div>
-    )
+import booksData from '../data/booksData';
+import Container from '../components/Container';
+import ToggleBtn from '../components/ToggleBtn';
+
+export default class About extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            visible: true,
+            data: []
+        }
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            data: booksData
+        })
+    }
+
+    handleToggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <section className={this.state.visible ? "container" : "container show"}>
+                    <h1>Димитър Брацов препуска със свещения дар на спомените</h1>
+                    <Container data={this.state.data} />
+                </section>
+                <ToggleBtn
+                    handleToggle={this.handleToggle}
+                    visible={this.state.visible}
+                />
+            </>
+        )
+    }
 }
