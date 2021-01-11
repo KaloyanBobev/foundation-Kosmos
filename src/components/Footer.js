0d801394b681527c4b10af5cec4f71c84b83b54a
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-11 14:47:01 +0000

    update footer with social media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
+import SocialFollow from './SocialFollow';
+
 export default class Footer extends Component {
     render() {
+        let dateNow = new Date;
         return (
-            <div>
-                Footer
-            </div>
+            <section className="footer-container">
+                <SocialFollow />
+                <h4>Всички права запазени. CopyRight ©  {dateNow.getFullYear()} година</h4>
+
+            </section>
         )
     }
 }
