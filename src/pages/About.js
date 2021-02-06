b99aca7d3c12d768a6894268669fcf1528a9ae25
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-06 18:07:09 +0000

    add image to the front of the page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 import booksData from '../data/booksData';
 import Container from '../components/Container';
 import ToggleBtn from '../components/ToggleBtn';
+import img from '../images/cosmos/uchrediteli-klub-Kosmos-23-13.jpg';
 
 export default class About extends React.Component {
     constructor() {
@@ -30,9 +31,11 @@ export default class About extends React.Component {
         return (
             <>
                 <section className={this.state.visible ? "container" : "container show"}>
-                    <h1>Учредиха клуб “Космос” в резиденция “Змейово”</h1>
-
-                    <Container data={this.state.data} />
+                    <div>
+                        <h1>Учредиха клуб “Космос” в резиденция “Змейово”</h1>
+                        <a href={img} target="_blank" rel="noreferrer"><img src={img} className="img" alt="founders" /></a>
+                        <Container data={this.state.data} />
+                    </div>
                 </section>
                 <ToggleBtn
                     handleToggle={this.handleToggle}
