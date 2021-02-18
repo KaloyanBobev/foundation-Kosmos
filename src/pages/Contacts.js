5364ae0623b44347a4a789a8c6631ff0ed0b9bbc
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-18 17:58:05 +0000

    add the semantic tags

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -4,11 +4,9 @@ export default function Contacts() {
     return (
         <section className="contacts-container">
             <h1>Фондация „Интелектуален клуб КОСМОС – Димитър Брацов“</h1>
-            <hr />
-            <h3>Aдрес: гр. Стара Загора, кв. „Три чучура-север“, бл. 79, ап. 01</h3>
-            <hr />
-            <h3>Mоб. тел.: 0898 679 609</h3>
-            <hr />
+            <h3><address>Aдрес: гр. Стара Загора, кв. „Три чучура-север“, бл. 79, ап. 01</address></h3>
+            <h3>Mоб. тел.:<phone> 0898 679 609</phone></h3>
+            <h3>Имейл:<email> bimed@mail.bg</email></h3>
             <h2>Contact form</h2>
         </section>
     )
