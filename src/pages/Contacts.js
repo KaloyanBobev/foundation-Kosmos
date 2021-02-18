d02757e8b56bb52304b8c9e2d215996b0518e363
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-18 13:14:23 +0000

    add some information

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,10 +3,12 @@ import React from 'react'
 export default function Contacts() {
     return (
         <section className="contacts-container">
-            <h1>Фондация Космос</h1>
-            <h3>address</h3>
-            <h3>tel.</h3>
-            <h3>mob.</h3>
+            <h1>Фондация „Интелектуален клуб КОСМОС – Димитър Брацов“</h1>
+            <hr />
+            <h3>Aдрес: гр. Стара Загора, кв. „Три чучура-север“, бл. 79, ап. 01</h3>
+            <hr />
+            <h3>Mоб. тел.: 0898 679 609</h3>
+            <hr />
             <h2>Contact form</h2>
         </section>
     )
