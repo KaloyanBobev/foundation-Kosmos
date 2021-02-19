ee50df2c3393cc60fc2a38e99f03f460b040d5fa
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-19 11:50:27 +0000

    split the information into two sections

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,12 @@ export default class About extends React.Component {
     constructor() {
         super();
         this.state = {
-            visible: true,
+            visible1: true,
+            visible2: true,
             data: []
         }
-        this.handleToggle = this.handleToggle.bind(this);
+        this.handleToggle1 = this.handleToggle1.bind(this);
+        this.handleToggle2 = this.handleToggle2.bind(this);
     }
 
     componentDidMount() {
@@ -21,26 +23,41 @@ export default class About extends React.Component {
         })
     }
 
-    handleToggle() {
+    handleToggle1() {
         this.setState({
-            visible: !this.state.visible
+            visible1: !this.state.visible1
         })
     }
 
+    handleToggle2() {
+        this.setState({
+            visible2: !this.state.visible2
+        })
+    }
     render() {
         return (
             <>
-                <section className={this.state.visible ? "container" : "container show"}>
+                <section className={this.state.visible1 ? "container" : "container show"}>
                     <div>
                         <h1>Учредиха клуб “Космос” в резиденция “Змейово”</h1>
                         <a href={img} target="_blank" rel="noreferrer"><img src={img} className="img" alt="founders" /></a>
-                        <Container data={this.state.data} />
+                        <Container data={this.state.data.slice(0, 7)} />
                     </div>
                 </section>
                 <ToggleBtn
-                    handleToggle={this.handleToggle}
-                    visible={this.state.visible}
+                    handleToggle={this.handleToggle1}
+                    visible={this.state.visible1}
                 />
+
+                <section className={this.state.visible2 ? "container" : "container show"}>
+                    <div>
+                        <h1> Димитър Брацов препуска със свещения дар на спомените</h1>
+                        <Container data={this.state.data.slice(8, 22)} />
+                    </div>
+                </section>
+                <ToggleBtn
+                    handleToggle={this.handleToggle2}
+                    visible={this.state.visible2} />
             </>
         )
     }
