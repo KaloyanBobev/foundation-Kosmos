55bdffb19412a94c4a34b3e17c2377af762816d7
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-19 18:01:05 +0000

    create new image data

---
+++ src/data/imageData.js
@@ -0,0 +1,81 @@
+import img1 from '../images/cosmos/130450932_110225667612487_2397097314206639749_o.jpg';
+import img2 from '../images/cosmos/Dimitar-Bracov.jpg';
+import img3 from '../images/cosmos/hristo-tanev-01-07-16-4.jpg';
+import img4 from '../images/cosmos/Jordan-Atanasov1.jpg';
+import img5 from '../images/cosmos/korica-ptici-v-noshta1-190x122.jpg';
+import img6 from '../images/cosmos/Liliev-konkurs-05-25-15-190x122.jpg';
+import img7 from '../images/cosmos/uchrediteli-klub-Kosmos-23-13.jpg';
+
+// eslint-disable-next-line
+
+export default [
+    {
+        sys: {
+            id: "1",
+
+        },
+        fields: {
+            name: "one",
+            image: img1
+        }
+    }, {
+        sys: {
+            id: "2",
+
+        },
+        fields: {
+            name: "Dimitar-Bracov",
+            text: img2
+        }
+    },
+    {
+        sys: {
+            id: "3",
+
+        },
+        fields: {
+            name: "hristo-tanev",
+            text: img3
+        }
+    },
+    {
+        sys: {
+            id: "4",
+
+        },
+        fields: {
+            name: "Jordan-Atanasov",
+            text: img4
+        }
+    },
+    {
+        sys: {
+            id: "5",
+
+        },
+        fields: {
+            name: "korica-ptici-v-noshta1",
+            text: img5
+        }
+    },
+    {
+        sys: {
+            id: "6",
+
+        },
+        fields: {
+            name: "Liliev-konkurs",
+            text: img6
+        }
+    },
+    {
+        sys: {
+            id: "7",
+
+        },
+        fields: {
+            name: "uchrediteli-klub-Kosmos",
+            text: img7
+        }
+    }
+]
