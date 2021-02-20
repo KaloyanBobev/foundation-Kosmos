5f0641f785ea7d1514be91600ef957841d3a25dc
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-20 10:34:28 +0000

    create the code into Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+
 
 export default class Image extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                {this.props.data.map((item) => {
+                    return (
+                        <a href={item.fields.src}
+                            target="_blank" rel="noreferrer">
+                            <img className="img" src={item.fields.src} alt={item.fields.name} />
+                        </a>
+                    )
+                })}
+            </>
         )
     }
 }
