91bf8858cd5ab3a01485734d2f0aec1977c65328
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-20 19:10:35 +0000

    add react imgix

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 
-
+import Imgix from 'react-imgix';
 
 export default class Image extends Component {
     render() {
         return (
             <>
-                {this.props.data.map((item) => {
+                {this.props.data.map(image => {
                     return (
-                        <a href={item.fields.src}
-                            target="_blank" rel="noreferrer">
-                            <img className="img" src={item.fields.src} alt={item.fields.name} />
-                        </a>
+                        <>
+                            <a href={image.fields.src}
+                                target="_blank" rel="noreferrer">
+                                <Imgix
+                                    className="img"
+                                    sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
+                                    src={image.fields.src}
+                                    imgixParams={{
+                                        fit: "crop",
+                                        fm: "jpg"
+                                    }}
+                                />
+                            </a>
+                        </>
                     )
                 })}
             </>
