5bdf948b18cfff00a126f118c87312c9669fcd37
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-22 11:50:24 +0000

    add the contact form component

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ContactForm from '../components/ContactForm';
 export default function Contacts() {
     return (
         <section className="contacts-container">
@@ -7,7 +7,8 @@ export default function Contacts() {
             <h3><address>Aдрес: гр. Стара Загора, кв. „Три чучура-север“, бл. 79, ап. 01</address></h3>
             <h3>Mоб. тел.:<phone> 0898 679 609</phone></h3>
             <h3>Имейл:<email> bimed@mail.bg</email></h3>
-            <h2>Contact form</h2>
+            <h3>Contact us</h3>
+            <ContactForm />
         </section>
     )
 }
