c1406f088bfb99e654e61ed594ed1c36c26019af
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-24 12:21:07 +0000

    remove some tags

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,8 +5,8 @@ export default function Contacts() {
         <section className="contacts-container">
             <h1>Фондация „Интелектуален клуб КОСМОС – Димитър Брацов“</h1>
             <h3><address>Aдрес: гр. Стара Загора, кв. „Три чучура-север“, бл. 79, ап. 01</address></h3>
-            <h3>Mоб. тел.:<phone> 0898 679 609</phone></h3>
-            <h3>Имейл:<email> bimed@mail.bg</email></h3>
+            <h3>Mоб. тел.: 0898 679 609</h3>
+            <h3>Имейл: bimed@mail.bg</h3>
             <h3>Contact us</h3>
             <ContactForm />
         </section>
