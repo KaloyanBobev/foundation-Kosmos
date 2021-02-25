e8e42d169f9c9f2a065ec0bf42acad051436c66d
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-25 13:43:21 +0000

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -10,15 +10,29 @@ export default class ContactForm extends Component {
             email: '',
             message: ''
         }
+        this.updateField = this.updateField.bind(this);
+    }
+
+    updateField(field, value) {
+        this.setState({ [field]: value });
     }
 
     render() {
         return (
             <div>
-                <Field onChange={ } value={this.state.name} />
-                <Field onChange={ } value={this.state.email} />
-                <Field textarea onChange={ } value={this.state.message} />
-                <Button formValues={this.state} email="kaloyanbodev@gmail.com" />
+                <Field onChange={(event) => this.updateField('name', event.target.value)}
+                    value={this.state.name} />
+                <Field onChange={(event) => this.updateField('email', event.target.value)}
+                    value={this.state.email} />
+                <Field
+                    onChange={(event) => this.updateField('message', event.target.value)}
+                    textarea={true}
+                    value={this.state.message} />
+                <Button
+
+                    email="kaloyanbodev@gmail.com"
+                    formValues={this.state}
+                />
 
             </div>
         )
