bb8d2125c210cca9ff0107c842e507e0f0cc4694
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-25 13:43:53 +0000

    create button component

---
+++ src/components/Button.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Button extends React.Component {
+
+    static propTypes = {
+        email: PropTypes.string.isRequired,
+        formValues: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            email: PropTypes.string.isRequired,
+            message: PropTypes.string.isRequired,
+        }).isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClicked: false
+        }
+    }
+
+    logFormDataToConsole(event) {
+        console.log(this.props.formValues)
+    }
+    render() {
+        const recepient = `mailto:${this.props.email}`;
+        const subject = `?subject=Interested%20Client`;
+        const body = `&body=${this.props.formValues.message}`;
+        return (
+            <a
+                href={`${recepient}${subject}${body}`}
+                onClick={this.logFormDataToConsole}
+            >
+                Contact us
+            </a>
+        )
+    }
+}
+
+export default Button;
