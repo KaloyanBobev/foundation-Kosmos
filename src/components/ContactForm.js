5d3e2bef8184a817e1f4eecbf80f8e595e818751
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-26 18:17:20 +0000

    add class,placeholder and label

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,22 +19,30 @@ export default class ContactForm extends Component {
 
     render() {
         return (
-            <div>
+            <div className="contact-form-container">
+                <h2>Може да се свържете с нас и чрез формата за контакти</h2>
                 <Field
-                    label={"Name"}
+                    placeholder={"Вашето име"}
+                    class={"field"}
+                    label={"Name: "}
                     onChange={(event) => this.updateField('name', event.target.value)}
                     value={this.state.name} />
                 <Field
-                    label={"Email"}
+                    placeholder={"Вашия имейл"}
+                    class={"field"}
+                    label={"Email: "}
                     onChange={(event) => this.updateField('email', event.target.value)}
                     value={this.state.email} />
                 <Field
-                    label={"Message"}
+                    placeholder={"Запишете вашето съобщение"}
+                    class={"message"}
+                    label={"Message: "}
                     onChange={(event) => this.updateField('message', event.target.value)}
                     textarea={true}
-                    value={this.state.message} />
+                    value={this.state.message}
+                    required={"required"}
+                />
                 <Button
-
                     email="canoncop2@abv.bg"
                     obfuscate={true}
                     formValues={this.state}
