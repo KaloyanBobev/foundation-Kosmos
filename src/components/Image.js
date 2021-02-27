5878bb0218987991cc26139021cf3cc0886f8620
kaloyanbobev <kaloyanbobev@gmail.com>
2021-02-27 22:00:13 +0000

    remove imgix

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
 
-import Imgix from 'react-imgix';
+
 
 export default class Image extends Component {
     render() {
         return (
             <>
-                {this.props.data.map(image => {
+                {this.props.data.map((image) => {
                     return (
-                        <>
-                            <a href={image.fields.src}
+                        <span key={image.sys.id}>
+                            <a
+                                href={image.fields.src}
                                 target="_blank" rel="noreferrer">
-                                <Imgix
+                                <img
                                     className="img"
-                                    sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
                                     src={image.fields.src}
-                                    imgixParams={{
-                                        fit: "crop",
-                                        fm: "jpg"
-                                    }}
+                                    alt={image.fields.name}
                                 />
                             </a>
-                        </>
+                        </span>
                     )
-                })}
+                })
+                }
             </>
         )
     }
