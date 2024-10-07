70adf649eeb7ba11e1ed5821ee1b230ebdcb6cf6
Kaloyan <kaloyanbobev@gmail.com>
2024-10-07 20:39:29 +0100

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,46 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
+// Creating a class-based component called Button
 class Button extends React.Component {
+  // Defining prop types for the component to ensure correct data types are passed
+  static propTypes = {
+    email: PropTypes.string.isRequired, // Email is required and should be a string
+    formValues: PropTypes.shape({
+      name: PropTypes.string.isRequired, // Name is required and should be a string
+      email: PropTypes.string.isRequired, // Email is required and should be a string
+      message: PropTypes.string.isRequired, // Message is required and should be a string
+    }).isRequired,
+  };
 
-    static propTypes = {
-        email: PropTypes.string.isRequired,
-        formValues: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            email: PropTypes.string.isRequired,
-            message: PropTypes.string.isRequired,
-        }).isRequired,
+  // Constructor to initialize the state and bind methods
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false, // Track whether the button has been clicked
     };
+    // Bind the logFormDataToConsole method to ensure it uses the correct 'this'
+    this.logFormDataToConsole = this.logFormDataToConsole.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isClicked: false
-        }
-        this.logFormDataToConsole = this.logFormDataToConsole.bind(this);
-    }
+  // Method to log form data to the console and update the state
+  logFormDataToConsole() {
+    console.log("Form Values", this.props.formValues); // Log form values to the console
+    this.setState({ isClicked: true }); // Set isClicked to true after the button is clicked
+  }
 
-    logFormDataToConsole() {
-        console.log('Form Values', this.props.formValues)
-        this.setState({ isClicked: true });
-    }
-    render() {
-        const recepient = `mailto:${this.props.email}`;
-        const subject = `?subject=Contact%20Form%20Fondacia%20Kosmos`;
-        const body = `&body=${this.props.formValues.message}`;
-        return (
+  render() {
+    // Create a mailto link with the recipient's email, subject, and body
+    const recepient = `mailto:${this.props.email}`;
+    const subject = `?subject=Contact%20Form%20Fondacia%20Kosmos`; // Subject for the email
+    const body = `&body=${this.props.formValues.message}`; // Include form message in the email body
 
-            <a href={`${recepient}${subject}${body}`} >
-                <button
-                    className="btn btn-primary"
-                    disabled={this.state.isClicked}
-                    onClick={this.logFormDataToConsole}>
-                    Contact us
-                </button>
-            </a>
-
-        )
-    }
+    return (
+      // Email link that triggers the email client with pre-filled details
+      <a href={`${recepient}${subject}${body}`}>
+        <button
+          className="btn btn-primary" // Button with Bootstrap 'btn-primary' class for styling
+          disabled={this.state.isClicked} // Disable the button after it's clicked
+          onClick={this.logFormDataToConsole}
+        >
+          {" "}
+          {/* Log form data when the button is clicked */}
+          Contact us
+        </button>
+      </a>
+    );
+  }
 }
 
 export default Button;
