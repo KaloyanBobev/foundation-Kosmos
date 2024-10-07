2211075fff249fd79fcd483d76cf6da63b780db0
Kaloyan <kaloyanbobev@gmail.com>
2024-10-07 20:39:35 +0100

    Update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,17 +1,20 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
 
 export default function Container({ data }) {
-    return (
-        <>
-            {data.map((item) => {
-                return (
-                    <p key={item.sys.id}>
-                        {item.fields.text}
-                    </p>
+  // Declare a state variable to hold the data, initially set to the passed `data` prop
+  const [items, setItems] = useState(data);
 
-                )
-            })}
-        </>
-    )
+  // Optional: Effect hook can be used if the `data` prop is dynamic and changes over time
+  useEffect(() => {
+    setItems(data); // Update state when `data` prop changes
+  }, [data]);
+
+  return (
+    <>
+      {/* Map over items in the state and render them as paragraphs */}
+      {items.map((item) => {
+        return <p key={item.sys.id}>{item.fields.text}</p>;
+      })}
+    </>
+  );
 }
